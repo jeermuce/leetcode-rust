ccb15b067030463c0f42cb4e21ca3db19a4d5d1c
jeermuce <ernesto_mc.dev@proton.me>
2026-07-25 15:16:40 -0600

    DRY it a little

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,3 +1,73 @@
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __test_solution_variant_case {
+    (
+        fn ($($pname:ident : $pty:ty),+ $(,)?) -> $out_ty:ty;
+        cases {
+            $( $case_name:ident : ( $($input:expr),+ $(,)? ) => $expected:expr ),+ $(,)?
+        }
+        body |$actual:ident, $expected_ident:ident| $body:block
+        impl $impl_name:ident => $func:path
+    ) => {
+        mod $impl_name {
+            use super::*;
+
+            #[rstest::rstest]
+            $(
+                #[case::$case_name($($input),+, $expected)]
+            )+
+            fn cases(
+                $(#[case] $pname: $pty,)+
+                #[case] expected: $out_ty,
+            ) {
+                let actual = $func($($pname),+);
+
+                let $actual = actual;
+                let $expected_ident = expected;
+
+                $body
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! test_solution_variant_case {
+    (
+        fn $params:tt -> $out_ty:ty;
+        cases $cases:tt
+        impl $impl_name:ident => $func:path
+    ) => {
+        $crate::__test_solution_variant_case! {
+            fn $params -> $out_ty;
+            cases $cases
+            body |actual, expected| {
+                assert_eq!(actual, expected);
+            }
+            impl $impl_name => $func
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! test_solution_variant_case_with {
+    (
+        fn $params:tt -> $out_ty:ty;
+        cases $cases:tt
+        assert $assertion:expr;
+        impl $impl_name:ident => $func:path
+    ) => {
+        $crate::__test_solution_variant_case! {
+            fn $params -> $out_ty;
+            cases $cases
+            body |actual, expected| {
+                ($assertion)(actual, expected);
+            }
+            impl $impl_name => $func
+        }
+    };
+}
+
 #[macro_export]
 macro_rules! test_solution_variants {
     (
@@ -16,32 +86,6 @@ macro_rules! test_solution_variants {
                 impl $impl_name => $func
             }
         )+
-    };
-}
-
-#[macro_export]
-macro_rules! test_solution_variant_case {
-    (
-        fn ($($pname:ident : $pty:ty),+ $(,)?) -> $out_ty:ty;
-        cases {
-            $( $case_name:ident : ( $($input:expr),+ $(,)? ) => $expected:expr ),+ $(,)?
-        }
-        impl $impl_name:ident => $func:path
-    ) => {
-        mod $impl_name {
-            use super::*;
-
-            #[rstest::rstest]
-            $(
-                #[case::$case_name($($input),+, $expected)]
-            )+
-            fn cases(
-                $(#[case] $pname: $pty,)+
-                #[case] expected: $out_ty,
-            ) {
-                assert_eq!($func($($pname),+), expected);
-            }
-        }
     };
 }
 
@@ -66,33 +110,5 @@ macro_rules! test_solution_variants_with {
                 impl $impl_name => $func
             }
         )+
-    };
-}
-
-#[macro_export]
-macro_rules! test_solution_variant_case_with {
-    (
-        fn ($($pname:ident : $pty:ty),+ $(,)?) -> $out_ty:ty;
-        cases {
-            $( $case_name:ident : ( $($input:expr),+ $(,)? ) => $expected:expr ),+ $(,)?
-        }
-        assert $assertion:expr;
-        impl $impl_name:ident => $func:path
-    ) => {
-        mod $impl_name {
-            use super::*;
-
-            #[rstest::rstest]
-            $(
-                #[case::$case_name($($input),+, $expected)]
-            )+
-            fn cases(
-                $(#[case] $pname: $pty,)+
-                #[case] expected: $out_ty,
-            ) {
-                let actual = $func($($pname),+);
-                ($assertion)(actual, expected);
-            }
-        }
     };
 }
